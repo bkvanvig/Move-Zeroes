ea0fddb46518cccfcfaac205c3b7d20b576cdd81
Brittany Kvanvig <brittany.kvanvig@utexas.edu>
2016-07-08 11:36:50 -0500

    removed extra declarations

--- Solution.java
+++ Solution.java
@@ -1,17 +1,16 @@
 public class Solution {
     public void moveZeroes(int[] nums) {
-        int countzero = 0;
         int writeIndex = 0;
         int len = nums.length;
         for (int i = 0; i < len; i++){
             if (nums[i]==0)
-                countzero++;
+                continue;
             else{
                 nums[writeIndex] = nums[i];
                 writeIndex++;
             }
         }
-        for(int i = len-1; i>=nums.length-countzero; i--){
+        for(int i = len-1; i>=writeIndex; i--){
             nums[i] = 0;
         }
     }
